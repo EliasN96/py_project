5d69b37c1c2df331221e02b6785a7e371ffdab33
Ilya Novikov <illianovikovnitzana@gmail.com>
2024-03-30 17:39:55 +0700

    main.py update substraction

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from funcs import sum_two_num
+from funcs import sum_two_num, subtraction
 
 
 def main():
     print(sum_two_num(2, 2))
+    print(subtraction(4, 2))
 
 
 if __name__ == "__main__":
